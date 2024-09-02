a3cf66b23abc7e94487423f00ebe2abfeb71f674
kauan <almeidakauan123@gmail.com>
2024-09-02 15:53:17 -0400

    melhorias FormDispositivos

--- INTERFACE-PROVIDER/src/components/client/EditClient/subcabeçalho/components/FormDispositivos/FormDispositivos.jsx
+++ INTERFACE-PROVIDER/src/components/client/EditClient/subcabeçalho/components/FormDispositivos/FormDispositivos.jsx
@@ -51,8 +51,69 @@ function FormDispositivos() {
             <h1>Fabricante</h1>
             <input type="text" placeholder="Digite o Serial" />
           </div>
+
         </div>
+
       </div>
+
+      <div className='FormDispositivos-3-0' >
+      <div className='FormDispositivos-3'>
+
+          <div className='FormDispositivos-3-1'>
+            <div className='FormDispositivos-3-2'>
+              <h1>Name SSID 2.4G</h1>
+              <input type="text" placeholder="Digite o nome da rede 2.4G" />
+            </div>
+
+            <div className='FormDispositivos-3-2'>
+              <h1>Password 2.4G</h1>
+              <input type="text" placeholder="Digite a Senha SSID 2.4G" />
+            </div>
+          </div>
+
+          <div className='FormDispositivos-3-1'>
+            <div className='FormDispositivos-3-2'>
+              <h1>Name SSID 5G</h1>
+              <input type="text" placeholder="Digite o nome da rede 5G" />
+            </div>
+
+            <div className='FormDispositivos-3-2'>
+              <h1>Password 5G</h1>
+              <input type="text" placeholder="Digite a Senha SSID 5G" />
+            </div>
+          </div>
+</div>
+
+        <div className='FormDispositivos-3'>
+
+        <div className='FormDispositivos-3-1'>
+          <div className='FormDispositivos-3-2'>
+            <h1>Name SSID 2.4G</h1>
+            <input type="text" placeholder="Digite o nome da rede 2.4G" />
+          </div>
+
+          <div className='FormDispositivos-3-2'>
+            <h1>Password 2.4G</h1>
+            <input type="text" placeholder="Digite a Senha SSID 2.4G" />
+          </div>
+        </div>
+
+        <div className='FormDispositivos-3-1'>
+          <div className='FormDispositivos-3-2'>
+            <h1>Name SSID 5G</h1>
+            <input type="text" placeholder="Digite o nome da rede 5G" />
+          </div>
+
+          <div className='FormDispositivos-3-2'>
+            <h1>Password 5G</h1>
+            <input type="text" placeholder="Digite a Senha SSID 5G" />
+          </div>
+        </div>
+        </div>
+
+
+</div>
+
     </div>
   );
 }
