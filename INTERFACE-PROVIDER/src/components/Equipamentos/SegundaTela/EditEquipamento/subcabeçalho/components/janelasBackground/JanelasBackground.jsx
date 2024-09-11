ed7d339725b3714e07d0a1282cf36bcf545dc91b
kauan <almeidakauan123@gmail.com>
2024-09-11 17:23:51 -0400

    melhorando css

--- INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/subcabeçalho/components/janelasBackground/JanelasBackground.jsx
+++ INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/subcabeçalho/components/janelasBackground/JanelasBackground.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './JanelasBackground.scss'
 function JanelasBackground( {children}) {
   return (
-    <div className='janelasBackground'>
+    <div className='janelasBackgrounddevice'>
          {children}
 
     </div>
