d0d9442b349369718bbc62f75febb18867e06782
kauan <almeidakauan123@gmail.com>
2024-09-12 17:31:43 -0400

    ajustando diagnostico

--- INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/subcabeçalho/components/FormDiagnosticos/components/MAIN/BOTÃO/Botao.jsx
+++ INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/subcabeçalho/components/FormDiagnosticos/components/MAIN/BOTÃO/Botao.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 import './Botao.scss';
 function Botao() {
   return (
-    <div className='Diagnosticos-Button'>
+    <div className='Diagnosticos-Ping'>
 
-        <button>butao 1{}</button>
-        <button>butao 2{}</button>
+        <div className='Diagnosticos-Ping-1'>
+              <h3>Endereço de destino:</h3>
+              <input type="number" name="" id="input-ip" />
+        </div>
+
+       <div  className='Diagnosticos-Ping-2'>
+            <div>
+                <h3>Repetições:</h3>
+                <input type="number" name="" id="" />
+            </div>
+
+
+            <div>
+                <h3>Timeout (ms):</h3>
+                <input type="number" name="" id="" />
+            </div>
+
+            <button id='btn-ping'>iniciar</button>
+     
+       </div>
+           
+    
         
     </div>
   )
