ed00d37663df6ee0ee72fd405ced80b72ae0e925
kauan <almeidakauan123@gmail.com>
2024-09-13 17:30:26 -0400

    ajustando header

--- INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/Cabeçalho/ComponentCabeçalho.jsx
+++ INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/Cabeçalho/ComponentCabeçalho.jsx
@@ -6,13 +6,43 @@ function ComponentCabeçalho() {
   return (
     <div className='ComponentCabeçalho-dispositivo'>
       
+      <div className='ComponentCabeçalho-dispositivo-1'>
+
       <div id='iconrouter'>
       <BsRouter style={{width : '80px', height: '70px',  padding: '10px'}}/>
+     
       </div>
    
-    <div>
-        <h1>scsdcd</h1>
-    </div>
+
+      </div>
+      
+   
+   
+
+    <div className='ComponentCabeçalho-dispositivo-2'>
+            <div >
+                <h3>id equipamentos</h3>
+            </div>
+            
+          <div>
+
+            <h3>usuario</h3>
+
+          </div>
+
+          <div>
+
+            <h3>serial</h3>
+
+          </div>
+
+          <div>
+
+            <h3>modelo</h3>
+
+          </div>
+    </div>   
+   
    
     </div>
   )
