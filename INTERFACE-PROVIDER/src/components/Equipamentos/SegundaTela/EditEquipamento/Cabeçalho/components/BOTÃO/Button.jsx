605701289cc4d8799208c8814a87822c42c6ce6c
kauan <almeidakauan123@gmail.com>
2024-09-14 10:48:59 -0400

    ajustando header equipamentos

--- INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/Cabeçalho/components/BOTÃO/Button.jsx
+++ INTERFACE-PROVIDER/src/components/Equipamentos/SegundaTela/EditEquipamento/Cabeçalho/components/BOTÃO/Button.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './Button.scss';
+import { FaTrashCan } from "react-icons/fa6";
+import { MdSync } from "react-icons/md";
+import { VscDebugRestart } from "react-icons/vsc";
+import { LuTimerReset } from "react-icons/lu";
 function Button() {
   return (
     <div className='ComponentCabeçalho-Button'>
         <div className='ComponentCabeçalho-Button-1'>
            
-            <button>Sincronizar</button>
-            <button>Apagar</button>
+            <button className='ComponentCabeçalho-Button-style'> <MdSync />Sincronizar</button>
+
+            <button id='ComponentCabeçalho-Button-trash'> <FaTrashCan /> Apagar</button>
         </div>
 
         <div className='ComponentCabeçalho-Button-2'>
-            <button>Reiniciar</button>
-            <button>Resetar</button>
+            <button className='ComponentCabeçalho-Button-style'><VscDebugRestart /> Reiniciar</button>
+            <button className='ComponentCabeçalho-Button-style'> <LuTimerReset /> Resetar</button>
         </div>
         
     </div>
